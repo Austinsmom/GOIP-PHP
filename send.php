60ac8dd54a7752616b03a4a7d3dadca427a80415
Eduardo Fontinelle <dudumiquim@gmail.com>
2016-01-07 14:52:48 -0200

    Change the script to send SMS using line command and taking off some HTTP headers

--- send.php
+++ send.php
@@ -1,59 +1,48 @@
 <?php
 /*
- * Usage: send JSON data as HTTP POST
- * example message:
- * {
- *    "receiver": "0121212123",
- *    "message": "Hello World!"
- * }
- * You may also set "id": 1234 (where 1234 is a unique ID) if you want to 
- * avoid collisions with multiple simultaneous sendings
- * 
+ * Usage: pass parameters through line command like example above
+ * example:
+ *    php send.php 0121212123 "Hello World!"
+ * @parameters receiver message
  * @author Iivo Raitahila
+ * @author Eduardo Fontinelle
+ * This project was perfectly written by Iivo Raitahila and forked and adapted to my use. Enjoy!
  */
 
+$debug = true;
 error_reporting(E_ALL);
+if ($debug) {
+    ini_set('display_errors', 1);
+} else {
+    ini_set('display_errors', 0);
+}
 
-/* Allow the script to hang around waiting for connections. */
-set_time_limit(0);
 
-/* Turn on implicit output flushing so we see what we're getting
- * as it comes in. */
-ob_implicit_flush();
+if (count($argv) != 3) {
+    exit("
+...................................................................
+* * Wrong use * *
+  Example how to use this script:
+
+$ php send.php <phone number> <message>
+
+................................................................... \n");
+}
 
 require_once './models/message.php';
 require_once './classes/goip.php';
 $settings = require 'settings.php';
 
-if($_SERVER['REQUEST_METHOD'] != "POST") {
-    header('HTTP/1.1 405 Method Not Allowed');
-    header('Allow: POST');
-    exit("405 Method Not Allowed");
-}
-
-$data = json_decode(file_get_contents('php://input'));
-
-if (is_null($data) || json_last_error() != JSON_ERROR_NONE) {
-    header('HTTP/1.1 400 Bad Request');
-    die("400 Bad Request, error with JSON-data decoding");
-}
-
-$message = new FSG\MessageVO(rand(1000, 9999), $data->receiver, $data->message);
-
-if(!empty($data->id)) {
-    $message->setId($data->id);
-}
+$message = new FSG\MessageVO(rand(1000, 9999), $argv[1], $argv[2]);
 
 $goip = new FSG\Goip($settings['goipAddress'], $settings['goipPort'], $settings['goipPassword']);
 
 $result = $goip->sendSMS($message);
 
 if($result === true) {
-    header('HTTP/1.1 200 OK');
+    echo 1;
 } else {
-    header('HTTP/1.1 400 Bad Request');
+    echo $result;
 }
-
-echo $result;
 
 $goip->close();
